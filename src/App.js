5abd734c30d61a77df64f0f45c9fd55a8c643cb8
SihyunSeo <63051137+SihyunSeo@users.noreply.github.com>
2021-01-26 18:29:17 +0900

    start_movie_project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import axios from 'axios';
+
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies : []
+  };
+  componentDidMount() {
+    const movies = axios.get('http://yts-proxy.now.sh/list_movies.json');
+  }
+  render() {
+    const {isLoading} = this.state;
+    return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>;
+  }
+}
+
+
+export default App;
